e8b7ccab69000ca74902b19583eb24bf88d5c307
CaioFML <caio.felipemotta@hotmail.com>
2021-11-13 01:22:56 -0300

    Adds contact validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,36 @@
 class Contact < ApplicationRecord
   belongs_to :user
+
+  before_validation :detect_franchise, :set_credit_card_last_four_digits
+
+  validates :name, :date_of_birth, :phone, :address, :credit_card, :email, :franchise, :credit_card_last_four_digits,
+            presence: true
+  validates :name,
+            format: { with: /\A[a-zA-Z\s-]+\z/, message: "Special characters is not allowed, you can use only \"-\"" }
+  validates :email, uniqueness: { scope: :user_id }, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/ }
+  validates :phone, format: { with: /\A(\(\+\d{2}\)\s\d{3}\s\d{3}\s\d{2}\s\d{2})|
+                                     (\(\+\d{2}\)\s\d{3}-\d{3}-\d{2}-\d{2})\z/x,
+                              message: "Only the formats (+00) 000 000 00 00 and (+00) 000-000-00-00 are allowed" }
+
+  validate :birthday_format
+
+  private
+
+  def birthday_format
+    Date.iso8601(date_of_birth)
+  rescue Date::Error
+    errors.add(:date_of_birth, "only the formats YYYYMMDD and YYYY-MM-DD are allowed")
+  end
+
+  def detect_franchise
+    brand_name = CreditCardValidations::Detector.new(credit_card).brand_name
+    self.franchise = brand_name
+  end
+
+  def set_credit_card_last_four_digits
+    return if credit_card.blank?
+
+    self.credit_card_last_four_digits = credit_card&.last(4)
+    self.credit_card = BCrypt::Password.create(credit_card)
+  end
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,4 +2,75 @@ describe Contact do
   describe "associations" do
     it { is_expected.to belong_to(:user) }
   end
+
+  describe "validations" do
+    subject(:contact) { described_class.new }
+
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :date_of_birth }
+    it { is_expected.to validate_presence_of :phone }
+    it { is_expected.to validate_presence_of :address }
+    it { is_expected.to validate_presence_of :credit_card }
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_presence_of :franchise }
+    it { is_expected.to validate_uniqueness_of(:email).scoped_to(:user_id) }
+    it { is_expected.to allow_value("email@addresse.foo").for(:email) }
+    it { is_expected.not_to allow_value("emailaddresse.foo").for(:email) }
+
+    it do
+      expect(contact).to allow_value("(+00) 000 000 00 00")
+        .for(:phone)
+        .with_message("Only the formats (+00) 000 000 00 00 and (+00) 000-000-00-00 are allowed")
+    end
+
+    it do
+      expect(contact).to allow_value("(+00) 000-000-00-00")
+        .for(:phone)
+        .with_message("Only the formats (+00) 000 000 00 00 and (+00) 000-000-00-00 are allowed")
+    end
+
+    it do
+      expect(contact).to allow_value("19960110")
+                           .for(:date_of_birth)
+                           .with_message("only the formats YYYYMMDD and YYYY-MM-DD are allowed")
+    end
+
+    it do
+      expect(contact).to allow_value("1996-01-10")
+                           .for(:date_of_birth)
+                           .with_message("only the formats YYYYMMDD and YYYY-MM-DD are allowed")
+    end
+
+    it do
+      expect(contact).to allow_value("Caio Motta")
+                           .for(:name)
+                           .with_message("Special characters is not allowed, you can use only \"-\"")
+    end
+
+    it do
+      expect(contact).to allow_value("Caio-Motta")
+                           .for(:name)
+                           .with_message("Special characters is not allowed, you can use only \"-\"")
+    end
+  end
+
+  describe "#before_validation" do
+    let(:contact) { build(:contact) }
+
+    before { contact.save! }
+
+    it "detects and save brand name by credit card number" do
+      expect(contact.franchise).to eq "Visa"
+    end
+
+    context "when credit card number is present" do
+      it "encrypts credit card number" do
+        expect(BCrypt::Password.new(contact.credit_card)).to eq "4111111111111111"
+      end
+
+      it "saves only last four numbers of credit card" do
+        expect(contact.credit_card_last_four_digits).to eq "1111"
+      end
+    end
+  end
 end
