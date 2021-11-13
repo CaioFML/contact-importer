4d2d4b282d8a2fd23da57f24c277f1823e487912
CaioFML <caio.felipemotta@hotmail.com>
2021-11-12 23:09:12 -0300

    Add enums to contact file

--- app/models/contact_file.rb
+++ app/models/contact_file.rb
@@ -1,5 +1,12 @@
 class ContactFile < ApplicationRecord
   has_one_attached :file
 
+  enum status: {
+    on_hold: "on hold",
+    processing: "processing",
+    failed: "failed",
+    finished: "finished"
+  }
+
   belongs_to :user
 end
--- spec/models/contact_file_spec.rb
+++ spec/models/contact_file_spec.rb
@@ -3,4 +3,11 @@ describe ContactFile do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to have_one_attached(:file) }
   end
+
+  describe "enums" do
+    it { expect(build(:contact_file, status: :on_hold)).to be_on_hold }
+    it { expect(build(:contact_file, status: :processing)).to be_processing }
+    it { expect(build(:contact_file, status: :failed)).to be_failed }
+    it { expect(build(:contact_file, status: :finished)).to be_finished }
+  end
 end
