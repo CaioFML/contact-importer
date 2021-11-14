955b2eefc6e7079a5eb3eb1acfb72a5b6158feed
CaioFML <caio.felipemotta@hotmail.com>
2021-11-13 22:48:31 -0300

    Adds navbar on layout

---
+++ spec/views/layouts/_navbar.html_spec.rb
@@ -0,0 +1,21 @@
+describe "layouts/_navbar.html.erb" do
+  before { render }
+
+  it "displays application name" do
+    expect(rendered)
+      .to have_selector "a.d-flex.align-items-center.mb-3.mb-md-0.me-md-auto.text-dark.text-decoration-none",
+                        text: "Contacts Importer"
+  end
+
+  it "displays link to home" do
+    expect(rendered).to have_link "Home", href: contacts_path
+  end
+
+  it "displays link to contacts" do
+    expect(rendered).to have_link "Contacts", href: contacts_path
+  end
+
+  it "displays link to contact files" do
+    expect(rendered).to have_link "Contacts files", href: contact_files_path
+  end
+end
