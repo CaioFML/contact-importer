a29c12296425c4f86dd44c50a730c1f0426e3554
CaioFML <caio.felipemotta@hotmail.com>
2021-11-14 01:36:46 -0300

    Fix offenses

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,15 +2,15 @@ describe ApplicationHelper do
   describe "create_select_options" do
     let(:select_input) do
       <<~SELECT
-      <select name="contact_file[columns][name]" class="form-select" required="true">
-        <option value=""></option>
-        <option value="0">One</option>
-        <option value="1">Two</option>
-        <option value="2">Three</option>
-        <option value="3">Four</option>
-        <option value="4">Five</option>
-        <option value="5">Six</option>
-      </select>
+        <select name="contact_file[columns][name]" class="form-select" required="true">
+          <option value=""></option>
+          <option value="0">One</option>
+          <option value="1">Two</option>
+          <option value="2">Three</option>
+          <option value="3">Four</option>
+          <option value="4">Five</option>
+          <option value="5">Six</option>
+        </select>
       SELECT
     end
 
