45e46a9e3008a55670e4f4a39af4cc6d7f0dd075
CaioFML <caio.felipemotta@hotmail.com>
2021-11-14 01:32:45 -0300

    Adds new and create actions on contact files controller

--- app/controllers/contact_files_controller.rb
+++ app/controllers/contact_files_controller.rb
@@ -3,7 +3,25 @@ class ContactFilesController < ApplicationController
     @contact_files = current_user.contact_files
   end
 
-  def new; end
+  def new
+    @contact_file = ContactFile.new
+  end
 
-  def create; end
+  def create
+    @contact_file = ContactFile.new(contact_file_params)
+
+    @contact_file.save
+
+    redirect_to contact_files_path, notice: "File was successfully import, wait for the contact importing processing!"
+  end
+
+  private
+
+  def contact_file_params
+    params.require("contact_file").permit(:file, :user_id)
+  end
+
+  def columns_params
+    params.require("columns").permit(:name, :date_of_birth, :phone, :address, :credit_card, :email)
+  end
 end
--- spec/requests/contact_files_request_spec.rb
+++ spec/requests/contact_files_request_spec.rb
@@ -1,6 +1,8 @@
 describe ContactFilesController do
   before { sign_in user }
 
+  let(:user) { create(:user) }
+
   describe "GET #index" do
     subject! { get contact_files_path }
 
@@ -13,6 +15,44 @@ describe ContactFilesController do
 
     it "displays content files" do
       expect(assigns(:contact_files)).to match_array(contact_files)
+    end
+  end
+
+  describe "GET #new" do
+    subject!(:get_new) { get new_contact_file_path }
+
+    it { expect(response).to have_http_status :ok }
+  end
+
+  describe "POST #create" do
+    subject(:post_create) { post contact_files_path, params: params }
+
+    let(:params) do
+      {
+        contact_file: { file: fixture_file_upload("valid_contacts.csv"), user_id: user.id },
+        columns: {
+          name: 0,
+          date_of_birth: 1,
+          phone: 2,
+          address: 3,
+          credit_card: 4,
+          email: 5
+        }
+      }
+    end
+
+    it "attaches uploaded file" do
+      expect { post_create }.to change(ActiveStorage::Attachment, :count).by(1)
+    end
+
+    it "creates contact file" do
+      expect { post_create }.to change(ContactFile, :count).by 1
+    end
+
+    it "displays flash notice" do
+      post_create
+
+      expect(flash[:notice]).to eq "File was successfully import, wait for the contact importing processing!"
     end
   end
 end
