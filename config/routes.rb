2b38aec6d4739a2a49e39a93efee7013253ca762
CaioFML <caio.felipemotta@hotmail.com>
2021-11-13 22:21:13 -0300

    Add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :contacts, only: :index
+  resources :contact_files, only: %i[index new create]
+
+  root to: "contacts#index"
 end
