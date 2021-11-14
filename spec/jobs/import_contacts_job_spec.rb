934c26a6155ead57be93d726992c91fedab936e4
CaioFML <caio.felipemotta@hotmail.com>
2021-11-14 04:31:44 -0300

    Fix test

--- spec/jobs/import_contacts_job_spec.rb
+++ spec/jobs/import_contacts_job_spec.rb
@@ -2,7 +2,7 @@ describe ImportContactsJob do
   describe "#perform" do
     subject(:perform) { described_class.perform_now(contact_file, column_params) }
 
-    let(:contact_file) { create(:contact_file) }
+    let(:contact_file) { create(:contact_file, file: fixture_file_upload("valid_contacts.csv")) }
     let(:column_params) do
       {
         name: "0",
@@ -14,16 +14,12 @@ describe ImportContactsJob do
       }
     end
 
-    it "updates contact file status" do
-      perform
-
-      expect(contact_file).to be_processing
-    end
-
-    it "cancels coupons" do
+    it "imports contact", :aggregate_failures do
       expect(ImportContacts).to receive(:call).with(contact_file: contact_file, column_params: column_params)
 
       perform
+
+      expect(contact_file).to be_processing
     end
   end
 end
