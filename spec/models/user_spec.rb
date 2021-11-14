9ae2b731155c6228580eeb24fd313b7be91037c3
CaioFML <caio.felipemotta@hotmail.com>
2021-11-13 22:56:32 -0300

    Adds association with user and contact files

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,6 @@
 describe User do
   describe "associations" do
     it { is_expected.to have_many(:contacts).dependent(:destroy) }
+    it { is_expected.to have_many(:contact_files).dependent(:destroy) }
   end
 end
