25dc9f9a37db70532bf2ac9e7775cc9cfc339eac
CaioFML <caio.felipemotta@hotmail.com>
2021-12-15 13:22:23 -0300

    Change job to use sidekiq worker instead of application job

--- spec/jobs/import_contacts_job_spec.rb
+++ spec/jobs/import_contacts_job_spec.rb
@@ -1,6 +1,6 @@
 describe ImportContactsJob do
   describe "#perform" do
-    subject(:perform) { described_class.perform_now(contact_file, column_params) }
+    subject(:perform) { described_class.new.perform(contact_file.id, column_params) }
 
     let(:contact_file) { create(:contact_file, file: fixture_file_upload("valid_contacts.csv")) }
     let(:column_params) do
@@ -20,6 +20,8 @@ describe ImportContactsJob do
       expect(ImportContacts).to receive(:call).with(contact_file: contact_file, column_params: column_params)
 
       perform
+
+      contact_file.reload
 
       expect(contact_file).to be_processing
     end
